98e61ffa0417cb3ec02ed3abe92f06c1332f6640
Ryan Brandt <60801464+BrandtRM@users.noreply.github.com>
2023-01-18 17:05:35 -0500

    Add files via upload

---
+++ Python/bs.py
@@ -0,0 +1,16 @@
+### Run the current code and understand the error message
+### Change only one line of the function bs and fix the error
+
+def bs(L, item, left = 0, right = None):
+    if right is None: right = len(L)
+    if right - left == 0: return False
+    if right - left == 1: return L[left] == item
+    median = (right + left)//2
+    if item < L[median]:
+        return bs(L, item, left, median)
+    else:
+        return bs(L, item, median, right)
+
+L = [i for i in range(10)]
+for i in range(20):
+    print(bs(L, i))
